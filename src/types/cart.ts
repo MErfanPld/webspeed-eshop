dff35b218a85e4b1d275af02f00e979d08ea7050
Mohammad Erfan Pouladzadeh <86192924+MErfanPld@users.noreply.github.com>
2026-09-17 10:47:34 +0200

    feat: add types, utils, store data and cart context

---
+++ src/types/cart.ts
@@ -0,0 +1,11 @@
+export type CartItem = {
+  id: string;
+  productId: string;
+  slug: string;
+  name: string;
+  price: number;
+  image: string;
+  size: string;
+  color: string;
+  quantity: number;
+};
